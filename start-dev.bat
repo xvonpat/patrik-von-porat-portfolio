566a78bbbb05c284b72d20a481e4d97550d51618
Patrik von Porat <xvonpat@gmail.com>
2026-08-14 00:40:51 +0200

    chore: add start-dev script for local development

---
+++ start-dev.bat
@@ -0,0 +1,8 @@
+@echo off
+title Portfolio Dev Server
+cd /d "%~dp0"
+echo ===================================================
+echo  Starting Patrik von Porat Portfolio Dev Server...
+echo ===================================================
+npm run dev
+pause
